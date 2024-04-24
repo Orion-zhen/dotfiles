5416b9163e6629c655e868a261026a49932de33b
Orion <public4orion@163.com>
2024-04-24 10:37:00 +0800

    feat: bluetoot tray-icon

--- install.sh
+++ install.sh
@@ -135,7 +135,7 @@ hyprland-install()
     echo "======================"
     echo "Installing Hyprland..."
     echo "======================"
-    sudo pacman -S hyprland hyprpaper waybar tofi kitty thunar mako swayidle swaylock cliphist hyprland-interactive-screenshot pamixer brightnessctl playerctl polkit-kde-agent udiskie network-manager-applet
+    sudo pacman -S hyprland hyprpaper waybar tofi kitty thunar mako swayidle swaylock cliphist hyprland-interactive-screenshot pamixer brightnessctl playerctl polkit-kde-agent udiskie network-manager-applet blueman
     yay -S hyprland-interactive-screenshot
 }
 
