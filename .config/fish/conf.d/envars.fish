3e86aef61f8b8ebec26ef0d28d07de1069316eaf
Orion <128988082+Orion-zhen@users.noreply.github.com>
2025-05-19 00:45:50 +0800

    :bug: set variables to global

--- .config/fish/conf.d/envars.fish
+++ .config/fish/conf.d/envars.fish
@@ -1,32 +1,32 @@
-set -x LANG zh_CN.UTF-8
+set -gx LANG zh_CN.UTF-8
 
 # fcitx5 输入法支持
-set -x GTK_IN_MODULE fcitx
-set -x QT_IM_MODULE fcitx
-set -x XMODIFIERS "@im=fcitx"
+set -gx GTK_IN_MODULE fcitx
+set -gx QT_IM_MODULE fcitx
+set -gx XMODIFIERS "@im=fcitx"
 
 # no_proxy 设置
-set -x no_proxy "localhost,127.0.0.1"
+set -gx no_proxy "localhost,127.0.0.1"
 
 if test -d /opt/cuda
-    set -x CUDA_HOME /opt/cuda
+    set -gx CUDA_HOME /opt/cuda
 end
 
 if test -d /opt/rocm
-    set -x ROCM_HOME /opt/rocm
+    set -gx ROCM_HOME /opt/rocm
 end
 
 if test -d $HOME/.cert
-    set -x SSL_HOME $HOME/.cert
+    set -gx SSL_HOME $HOME/.cert
 end
 
 if test -d $HOME/ai/Models
-    set -x MODELS $HOME/ai/Models
+    set -gx MODELS $HOME/ai/Models
 end
 
-set -x GPG_TTY $(tty)
+set -gx GPG_TTY $(tty)
 
-# set -x HOMEBREW_NO_INSTALL_CLEANUP false
+# set -gx HOMEBREW_NO_INSTALL_CLEANUP false
 
 # export HOMEBREW_API_DOMAIN
 # export HOMEBREW_BOTTLE_DOMAIN
