49959ceab1c1d34bac81abac43267a421d8e978e
Orion <128988082+Orion-zhen@users.noreply.github.com>
2025-05-19 00:46:42 +0800

    :bug: add path globaly

--- .config/fish/conf.d/path.fish
+++ .config/fish/conf.d/path.fish
@@ -1,11 +1,11 @@
-set -x PATH $PATH $HOME/.local/bin
+set -gx PATH $PATH $HOME/.local/bin
 
 if test -d /opt/cuda
-    set -x PATH $PATH /opt/cuda/bin
+    set -gx PATH $PATH /opt/cuda/bin
 end
 
 if test -d /opt/rocm
-    set -x PATH $PATH /opt/rocm/bin
+    set -gx PATH $PATH /opt/rocm/bin
 end
 
 # 检查 $HOME/opt 是否存在
@@ -16,12 +16,12 @@ if test -d "$HOME/opt"
         if test -d "$sub"
             # 如果存在 bin 子目录，则将其添加到 PATH 中
             if test -d "$sub/bin"
-                set -x PATH $sub/bin $PATH
+                set -gx PATH $sub/bin $PATH
             end
             # 如果存在 lib 子目录，则将其添加到 LD_LIBRARY_PATH 中
             if test -d "$sub/lib"
-                set -x LD_LIBRARY_PATH $sub/lib $LD_LIBRARY_PATH
-                set -x DYLD_LIBRARY_PATH $sub/lib $DYLD_LIBRARY_PATH
+                set -gx LD_LIBRARY_PATH $sub/lib $LD_LIBRARY_PATH
+                set -gx DYLD_LIBRARY_PATH $sub/lib $DYLD_LIBRARY_PATH
             end
         end
     end
